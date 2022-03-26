9b86e1015e6a7150f102dee1d919f23250d753b8
Kovida <mothukurikovidasree@gmail.com>
2022-03-26 21:35:15 +0000

    setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-imp
+from setuptools import setup, find_packages
+
+setup(
+	name='project1',
+	version='1.0',
+	author='Kovida Mothukuri',
+	authour_email='kovida.mothukuri-1@ou.com',
+	packages=find_packages(exclude=('tests', 'docs')),
+	setup_requires=['pytest-runner'],
+	tests_require=['pytest']	
+)
